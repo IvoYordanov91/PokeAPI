7531c8b40413274f843a18e443aba3992f2a3194
Ivo Yordanov <ivosvetcavicata@gmail.com>
2023-04-19 00:30:08 +0200

    Fix top img

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,12 @@ const inicio$$ = document.querySelector(".inicio");
 
 //Landing page build
 const landing = () => {
+  const topImg = document.createElement("section");
+  topImg.innerHTML = `
+    <article>
+    <img src="./styles/assets/img/banner.png" alt="bannerPokedex" class="banner">
+    </article>
+  `;
   const titleSection = document.createElement("section");
   titleSection.setAttribute("class", "titlesection");
   titleSection.innerHTML = `
@@ -33,6 +39,7 @@ const landing = () => {
 <p>¡Atrapalos a todos!</p>
 </article>
 `;
+  main$$.appendChild(topImg);
   main$$.appendChild(titleSection);
   main$$.appendChild(videoSection);
   main$$.appendChild(imgSection);
@@ -41,58 +48,57 @@ const landing = () => {
 //Build Pokemon Datalayer
 //Get first page
 const getFirstPagePokemon = async () => {
- try{   
+  try {
     const pokemons = [];
-  for (let i = 1; i < 151; i++) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    const res = await response.json();
-    let pokNameFirstLetter = res.name[0].toUpperCase();
-    let pokNameRest = res.name.slice(1);
-    let pokName = pokNameFirstLetter + pokNameRest;
-    let pokType = res.types.map((type) => type.type.name).toString();
-    const pokemon = {
+    for (let i = 1; i < 151; i++) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const res = await response.json();
+      let pokNameFirstLetter = res.name[0].toUpperCase();
+      let pokNameRest = res.name.slice(1);
+      let pokName = pokNameFirstLetter + pokNameRest;
+      let pokType = res.types.map((type) => type.type.name).toString();
+      const pokemon = {
         name: pokName,
         typeClass: pokType.replace(",", " "),
         image: res.sprites["front_default"],
-        type: pokType
-    }    
-    pokemons.push(`
+        type: pokType,
+      };
+      pokemons.push(`
     <article class="card">
     <h3 class="card-title">${pokemon.name}</h3>
     <img src="${pokemon.image}" alt="${pokemon.name}" class="card-image"></img>
     <p class="${pokemon.typeClass} card-subtitle">${pokemon.type}</p>
     </article>
-    `); 
-}
-  return pokemons; 
-} catch (error) {
+    `);
+    }
+    return pokemons;
+  } catch (error) {
     console.log("Se ha producido el siguiente error:", error);
-}
+  }
 };
 
 const drawPokemons = (pokemons) => {
-    if (main$$.children.length < 2) {
+  if (main$$.children.length < 2) {
     const sectionPokemons = document.createElement("section");
     sectionPokemons.setAttribute("id", "pokedex");
     sectionPokemons.innerHTML = pokemons;
     main$$.appendChild(sectionPokemons);
-    } else {
-        const pokeSection$$ = document.querySelector("#pokedex");
-        pokedex$$.innerHTML = pokemons;
-    }
+  } else {
+    const pokeSection$$ = document.querySelector("#pokedex");
+    pokedex$$.innerHTML = pokemons;
+  }
 };
 const searchEngine = (arr, pattern) => {
-    let results = arr.filter((pokemon) => {
-        pokemon.name.toLowerCase().includes(pattern.toLowerCase());
-    });
-    drawPokemons(results);
+  let results = arr.filter((pokemon) => {
+    pokemon.name.toLowerCase().includes(pattern.toLowerCase());
+  });
+  drawPokemons(results);
 };
 const takeInput = (userInput) => {
-    input$$.addEventListener("input", () => 
-        searchEngine(pokemons, input$$.value)
-    );
+  input$$.addEventListener("input", () =>
+    searchEngine(pokemons, input$$.value)
+  );
 };
-
 
 const init = async () => {
   landing();
@@ -107,14 +113,22 @@ const init = async () => {
     drawPokemons(pokemons);
     const pokePs$$ = document.getElementsByClassName("card-subtitle");
     for (let i = 0; i < pokePs$$.length; i++) {
-        if (pokePs$$[i].innerHTML.indexOf(",") !== -1) {
-            pokePs$$[i].innerHTML = `
+      if (pokePs$$[i].innerHTML.indexOf(",") !== -1) {
+        pokePs$$[i].innerHTML = `
             <div class="pcontainer">
-            <p class="${pokePs$$[i].innerText.split(",")[0]} card-subtitle multiple-type">${pokePs$$[i].innerText.split(",")[0]}</p>
-            <p class="${pokePs$$[i].innerText.split(",")[1]} card-subtitle multiple-type">${pokePs$$[i].innerText.split(",")[1]}</p>
+            <p class="${
+              pokePs$$[i].innerText.split(",")[0]
+            } card-subtitle multiple-type">${
+          pokePs$$[i].innerText.split(",")[0]
+        }</p>
+            <p class="${
+              pokePs$$[i].innerText.split(",")[1]
+            } card-subtitle multiple-type">${
+          pokePs$$[i].innerText.split(",")[1]
+        }</p>
             </div>
-            `;            
-        }
+            `;
+      }
     }
   });
   inicio$$.addEventListener("click", () => {
